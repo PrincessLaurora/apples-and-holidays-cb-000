d101acc3c736a7925735e3e4d0945549bc36f4a9
Laura Felce <crepu83@yahoo.it>
2018-11-27 14:52:13 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -29,7 +29,7 @@ def all_supplies_in_holidays(holiday_hash)
   holiday_hash.each do |season, holidays|
     puts "#{season.to_s.capitalize!}:"
     holidays.split each do |holiday, supplies|
-      puts "#{holiday.to_s.split('_').collect {|word|.capitalize!}.join(" ")}: #{supplies.join(", ")}"
+      puts "#{holiday.to_s.split('_').collect {|word|.capitalize!}.join(' ')}: #{supplies.join(", ")}"
   end
 end
 end
