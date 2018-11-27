35e100f8e835d74f12f062df886c8c1d0c1879b9
Laura Felce <crepu83@yahoo.it>
2018-11-27 15:31:54 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -47,8 +47,7 @@ def all_holidays_with_bbq(holiday_hash)
 holiday_hash.collect do |season, holidays|
 holidays.collect do |holiday, supplies|
  if supplies.include? ("BBQ")
-      return holidays.keys.compact
-
+      return .flatten.compact
 end
 end
 end
